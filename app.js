4eee7cc9dd299d6dda482d13bf3ed55f917cd644
Margo Drozd <drozdmargo@i.ua>
2016-12-19 14:31:00 +0200

    using yargs for getting address

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+.options({
+	a: {
+		demand: true,
+		alias: 'address',
+		describe: 'Address to fetch weather for',
+		string: true
+	}
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
+	console.log(`Address: ${body.results[0].formatted_address}`);
 	console.log(`lat: ${body.results[0].geometry.location.lat}`);
 	console.log(`lng: ${body.results[0].geometry.location.lng}`);
 });
